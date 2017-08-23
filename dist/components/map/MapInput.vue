9e2fa370197394771c91166dd502055e696ed847
Ian Rothmann <ian@afriforte.co.za>
2017-08-23 10:43:34 +0200

    no message

--- dist/components/map/MapInput.vue
+++ dist/components/map/MapInput.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div ref="main">
         <v-subheader style="padding-left:0;" v-if="label">{{label}}</v-subheader>
         <div v-if="error" class="input-group input-group--error radio input-group--selection-controls no-margins">
             <div class="input-group__details">
@@ -30,7 +30,25 @@
 
         </v-toolbar>
 
-        <v-card v-if="innerValue!=null" class="map-info">
+            <v-menu v-if="innerValue!=null" v-show="hideInfo" class="map-info hidden">
+                <v-btn slot="activator" icon><v-icon>info</v-icon></v-btn>
+                <v-card>
+                    <div style="padding:5px">
+                        <s-grid>
+                            <s-cell sm="4">
+                                <b>Marker position</b>
+                                <div>{{parseFloat(innerValue.lat).toFixed(5)}}, {{parseFloat(innerValue.lng).toFixed(5)}}</div>
+                            </s-cell>
+                            <s-cell sm="8" v-if="markeraddress!==''">
+                                <b>Closest matching address</b>
+                                <div>{{markeraddress}}</div>
+                            </s-cell>
+                        </s-grid>
+                    </div>
+                </v-card>
+            </v-menu>
+
+        <v-card v-if="innerValue!=null" class="map-info window" v-show="!hideInfo">
             <div style="padding:5px">
                 <s-grid>
                     <s-cell sm="4">
@@ -39,7 +57,7 @@
                     </s-cell>
                     <s-cell sm="8" v-if="markeraddress!==''">
                         <b>Closest matching address</b>
-                        <div id="address">{{markeraddress}}</div>
+                        <div>{{markeraddress}}</div>
                     </s-cell>
                 </s-grid>
             </div>
@@ -54,6 +72,7 @@
     </div>
 </template>
 <style>
+
     .input-group.no-margins{
         padding-top:0;
     }
@@ -66,14 +85,22 @@
         top:12px;
         left:calc(25%);
     }
-    .map-info{
+    .map-info.window{
         width:60%;
         font-size: 12px;
         position: absolute;
-        top:calc(80%);
+        top:calc(70%);
         left:calc(25%);
         opacity : 0.8;
     }
+
+    .map-info.hidden{
+        width:10%;
+        position: absolute;
+        top:calc(80%);
+        left:calc(45%);
+    }
+
     .map-ins{
         width:30%;
         font-size: 12px;
@@ -95,7 +122,7 @@
             name : {type:String},
             value : {},
             label : {},
-            center : {required:true,type:Object},
+            center : {type:Object},
             zoom : {type:Number, default:16},
             mode : {type:String,default:'edit'},
             height : {type:String,default:'300px'}
@@ -103,8 +130,13 @@
         inject: ['$validator'],
         computed:{
            innerCenter(){
-               if(this.innerValue===null){
-                    return this.center;
+               if(this.innerValue===null&&!this.center){
+                   this.innerZoom=1;
+                   return {
+                        lat:35.749568, lng:15.118623
+                    };
+               }else if(this.innerValue===null){
+                   return this.center;
                }else{
                    return this.innerValue;
                }
@@ -116,6 +148,17 @@
                if(this.name&&this.verrors&&this.verrors.has(this.name)){
                    return this.verrors.first(this.name);
                }
+           },
+           extendedInnerValue(){
+               if(this.innerValue===null){
+                   return null;
+               }else{
+                   return {
+                       lat:this.innerValue.lat,
+                       lng:this.innerValue.lng,
+                       address:this.markeraddress.indexOf('Cannot determine address')>=0?null:this.markeraddress
+                   }
+               }
            }
 
         },
@@ -126,20 +169,40 @@
                markeraddress : '',
                searchAddress : '',
                gMap : null,
-               gMarker : null
+               gMarker : null,
+               hideInfo : true,
+               innerZoom : 16
            }
         },
         mounted(){
+
            this.initMap();
            this.transferValue();
            this.$watch('value',()=>{
                this.transferValue();
            },{deep:true});
-
+           this.transferZoom();
+           this.$watch('value',()=>{
+                this.transferZoom();
+           });
+           this.processResize();
+           window.addEventListener('resize',()=>{
+               this.processResize();
+           }, true);
 
         },
+        beforeDestroy(){
+           // window.removeEventListener('resize',this.processResize());
+        },
         methods : {
-           transferValue(){
+            transferZoom(){
+               this.innerZoom=this.zoom;
+            },
+            processResize(){
+               let w=this.$refs.main.clientWidth;
+               this.hideInfo=w<550;
+            },
+            transferValue(){
                if(this.value!==null && typeof this.value === 'object'){
                    this.innerValue={
                        lat : this.value.lat,
@@ -150,6 +213,7 @@
                    }
                    let latLng=new google.maps.LatLng(this.innerValue.lat,this.innerValue.lng);
                    this.setMarkerPosition(latLng);
+                   this.findMarker();
                }else{
                    this.innerValue=null;
                    this.drawMarker();
@@ -160,7 +224,7 @@
 
                const mapOptions = {
                    center: new google.maps.LatLng(this.innerCenter.lat,this.innerCenter.lng),
-                   zoom: this.zoom
+                   zoom: this.innerZoom
                };
 
                this.gMap = new google.maps.Map(this.$refs.mapContainer,mapOptions);
@@ -178,7 +242,9 @@
            },
            emitInnerValue(){
                this.$emit('input',this.innerValue);
-               this.$emit('change',this.innerValue);
+           },
+           emitExtendedValue(){
+               this.$emit('change',this.extendedInnerValue);
            },
            clear(){
                this.innerValue=null;
@@ -203,6 +269,13 @@
                            this.updateInnerValue(this.gMarker.getPosition());
                        });
 
+                       google.maps.event.addListener(this.gMarker, 'rightclick', ()=>{
+                           this.gMarker.setMap(null);
+                           this.innerValue=null;
+                           this.gMarker=null;
+                           this.emitInnerValue();
+                       });
+
                        google.maps.event.addListener(this.gMarker, 'dragend', ()=>{
                            this.updateInnerValue(this.gMarker.getPosition());
                            this.setMarkerPosition(this.gMarker.getPosition());
@@ -222,6 +295,7 @@
                 }, (responses)=>{
                     if (responses && responses.length > 0) {
                         this.markeraddress=responses[0].formatted_address;
+                        this.emitExtendedValue();
                     } else {
                         this.markeraddress='Cannot determine address at this location.';
                     }
@@ -241,9 +315,15 @@
 
            },
            setMarkerPosition(latLng){
+               if(!this.gMarker){
+                   this.drawMarker();
+
+               }
                this.gMarker.setPosition(latLng);
+
                setTimeout(()=>{
                    this.geocodePosition(latLng);
+
                },50);
 
            },
@@ -256,6 +336,7 @@
                             this.updateInnerValue(responses[0].geometry.location);
                             this.setMarkerPosition(responses[0].geometry.location);
                             this.findMarker();
+                            this.emitInnerValue();
                             this.markeraddress=responses[0].formatted_address;
                         } else {
                             this.markeraddress='Cannot determine address at this location.';
